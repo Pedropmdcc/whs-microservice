15614bef36db848917b94f19273026cfc0f57680
Pedro Marques <Pedro.Marques@ctw.bmwgroup.com>
2019-10-23 17:28:36 +0100

    New Configs

---
+++ service-registry/src/test/java/com/whs/registry/RegistryApplicationTests.java
@@ -0,0 +1,9 @@
+package com.whs.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+}
