a8e18b5d37147424964343bfda88b813931f5090
Dieferson Matos <diefersonamatos@gmail.com>
2019-10-28 16:15:20 +0000

    CTWHS: Gateway Implementation
    Gateway Implementation

--- warehouse/src/main/java/com/whs/warehouse/infrastructure/material/model/Material.java
+++ warehouse/src/main/java/com/whs/warehouse/infrastructure/material/model/Material.java
@@ -1,9 +1,6 @@
 package com.whs.warehouse.infrastructure.material.model;
 
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -11,14 +8,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Material {
     @Id
     private String id;
     private String name;
     private String description;
 
-    public Material(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
 }
